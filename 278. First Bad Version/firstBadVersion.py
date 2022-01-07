40ce0de6c2faccf0e3bb6d87cb37f8d141eac84a
Shane <liushane84@gmail.com>
2022-01-07 23:05:50 +1100

    Update firstBadVersion.py

--- 278. First Bad Version/firstBadVersion.py
+++ 278. First Bad Version/firstBadVersion.py
@@ -4,39 +4,39 @@
 # def isBadVersion(version):
 
 from typing import List
+
 class Solution:
     def firstBadVersion(self, n):
         """
         :type n: int
         :rtype: int
         """
-        nums = list(range(n+1))
-        self.ternary(nums, 1, n)
+        return self.binary(1, n)
 
     def binary(self, l: int, r: int) -> int:
         if l <= r:
             mid = int((l + r) / 2)
-            if isFirstBadVersion(mid):
+            if self.isFirstBadVersion(mid):
                 return mid
             elif isBadVersion(mid):
-                return  self.binary(l, mid - 1)
+                return self.binary(l, mid - 1)
             else:
                 return self.binary(mid + 1, r)
         else:
             return -1
 
-        def isFirstBadVersion(n):
-            if n-1>0:
-                if not isBadVersion(n-1) and isBadVersion(nums[n]):
-                    return True
-                else:
-                    return False
+    def isFirstBadVersion(self, n):
+        if n - 1 > 0:
+            if not isBadVersion(n - 1) and isBadVersion(n):
+                return True
             else:
-                if isBadVersion(n):
-                    return True
-                else:
-                    print("There are not bad version.")
-                    return False
+                return False
+        else:
+            if isBadVersion(n):
+                return True
+            else:
+                print("There are not bad version.")
+                return False
 
 def isBadVersion(n):
     if n >= 3:
