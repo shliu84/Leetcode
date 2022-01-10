b14a00abab58e99c211c773486fbbd87656ed503
Shane <liushane84@gmail.com>
2022-01-10 23:11:33 +1100

    q35 solved

--- 35. Search Insert Position/Solution35.py
+++ 35. Search Insert Position/Solution35.py
@@ -1,12 +1,14 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         (l, r) = (0, len(nums) - 1)
-        if self.isBetween(nums, l, r, target) != -1:
-            return self.isBetween(nums, l, r, target)
-        else:
-            return self.binary(nums, target, l, r)
+        return self.binary(nums, target, l, r)
 
     def binary(self, nums: List[int], target: int, l: int, r: int) -> int:
+        if target <= nums[l]:
+            return l
+        if target > nums[r]:
+            return r + 1
+
         if l < r:
             mid = int((l + r) / 2)
             if nums[mid] == target:
@@ -16,16 +18,4 @@ class Solution:
             else:
                 return self.binary(nums, target, l, mid - 1)
         else:
-            return l + 1
-
-    def isBetween(self, nums, l, r, target):
-        if r - l < 2:
-            if nums[l] < target and target < nums[r]:
-                return r
-            if target < nums[l]:
-                return l
-            if target > nums[r]:
-                return r + 1
-        else:
             return -1
-
