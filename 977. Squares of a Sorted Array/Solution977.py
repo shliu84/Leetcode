72705f3e2017106784a53cd786c894f737a85ba0
Shane <liushane84@gmail.com>
2022-01-12 23:40:18 +1100

    solution 1 for q977
    Runtime: 300 ms, faster than 30.11% of Python3 online submissions for Squares of a Sorted Array.
    Memory Usage: 16.4 MB, less than 12.73% of Python3 online submissions for Squares of a Sorted Array.

--- 977. Squares of a Sorted Array/Solution977.py
+++ 977. Squares of a Sorted Array/Solution977.py
@@ -1,3 +1,19 @@
-class Solution
-    def sortedSquares(self, nums List[int]) - List[int]
-        
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        l = 0
+        r = len(nums)-1
+        arr=[]
+        arr = [-1] * len(nums)
+        for i in range(r,-1,-1):
+            if l == r:
+                arr[i] = nums[l]*nums[l]
+                break
+            (l,r,sq) = self.nextSquare(nums,l,r)
+            arr[i] = sq
+        return arr
+    
+    def nextSquare(self, nums, l, r):
+        if abs(nums[l]) < abs(nums[r]):
+            return (l, r-1, nums[r]*nums[r])
+        else:
+            return (l+1, r, nums[l]*nums[l])
