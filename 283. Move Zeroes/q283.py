cbaf1d40405a58becec49ae2339b1f2fde527854
Shane <liushane84@gmail.com>
2022-01-17 08:15:21 +1100

    q283 solved
    Runtime: 262 ms, faster than 30.46% of Python3 online submissions for Move Zeroes.
    Memory Usage: 15.3 MB, less than 91.05% of Python3 online submissions for Move Zeroes.

--- 283. Move Zeroes/q283.py
+++ 283. Move Zeroes/q283.py
@@ -3,4 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
+        left = 0
+        right = 0
+        while right < len(nums):
+                
+            if nums[left] == 0:
+                nums[left], nums[right] = nums[right], nums[left]
+            if nums[left] != 0:
+                left = left + 1
+            right = right + 1
