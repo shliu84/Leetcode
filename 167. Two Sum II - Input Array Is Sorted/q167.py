5ce477557769f68083809f091665e8ea37794718
Shane <liushane84@gmail.com>
2022-01-20 19:34:17 +1100

    solved q167
    Runtime: 103 ms, faster than 25.94% of Python3 online submissions for Two Sum II - Input Array Is Sorted.
    Memory Usage: 14.8 MB, less than 34.64% of Python3 online submissions for Two Sum II - Input Array Is Sorted.

--- 167. Two Sum II - Input Array Is Sorted/q167.py
+++ 167. Two Sum II - Input Array Is Sorted/q167.py
@@ -1,3 +1,14 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
+        l=0
+        r=len(numbers)-1
+        while l<r:
+            right = target-numbers[l]
+            if right==numbers[r]:
+                return l+1,r+1
+            if right<numbers[r]:
+                r = r -1
+            else:
+                if r+1 < len(numbers):
+                    r = r + 1
+                l = l + 1
