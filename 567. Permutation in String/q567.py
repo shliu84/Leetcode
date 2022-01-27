1a5d75f7bbea65f5c1674cbb18f1d2871d6f8809
Shane <liushane84@gmail.com>
2022-01-27 23:15:23 +1100

    Update q567.py
    Runtime: 7302 ms, faster than 5.01% of Python3 online submissions for Permutation in String.
    Memory Usage: 13.9 MB, less than 99.97% of Python3 online submissions for Permutation in String.

--- 567. Permutation in String/q567.py
+++ 567. Permutation in String/q567.py
@@ -1,3 +1,26 @@
+import collections
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         
+        def same_permutation(a, b):
+            d = collections.defaultdict(int)
+            for x in a:
+                d[x] += 1
+            for x in b:
+                d[x] -= 1
+            return not any(d.values())
+        
+        # return same_permutation(s1,s2[0:3])
+        
+        l = len(s1)
+        set1 = set(s1)
+        for i in range(len(s2)-l + 1):
+            if s2[i] in set1:
+                if same_permutation(s1, s2[i:i+l]):
+                    return True
+            else:
+                i = i + l
+        return False
+    
+    
+
