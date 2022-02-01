b4f9933be86a04359acda48c1af4657465ec0281
Shane <liushane84@gmail.com>
2022-02-01 13:06:36 +1100

    Update q733.py
    Runtime: 80 ms, faster than 61.97% of Python3 online submissions for Flood Fill.
    Memory Usage: 14 MB, less than 97.72% of Python3 online submissions for Flood Fill.

--- 733. Flood Fill/q733.py
+++ 733. Flood Fill/q733.py
@@ -2,19 +2,21 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
         
-        def dfs (image, r, c, color, newColor):
-            
-            if image[r][c] == color:
+        def dfs (image, r, c, color, newColor, arr):
+
+            if image[r][c] == color and arr[r][c] == 0:
                 image[r][c] = newColor
+                arr[r][c] = 1
                 if r > 0: 
-                    dfs(image, r-1,c,color,newColor)
+                    dfs(image, r-1,c,color,newColor, arr)
                 if r < len(image)-1: 
-                    dfs(image, r+1,c,color,newColor)
+                    dfs(image, r+1,c,color,newColor, arr)
                 if c > 0: 
-                    dfs(image, r,c-1,color,newColor)
+                    dfs(image, r,c-1,color,newColor, arr)
                 if c < len(image[0])-1: 
-                    dfs(image, r,c+1,color,newColor)
-            
+                    dfs(image, r,c+1,color,newColor, arr)
+
+        arr = [ [0]*len(image[0]) for i in range(len(image))]
         if image[sr][sc] != newColor:
-            dfs(image, sr,sc, image[sr][sc],newColor)
+            dfs(image, sr,sc, image[sr][sc],newColor, arr)
         return image
